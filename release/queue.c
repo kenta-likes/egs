81c97407c64453bfdc70087202d0d3d3e443c211
kentalabur <kl459@cornell.edu>
2014-09-10 18:53:31 -0400

    Queueueueueue is mostly done (minus iteration and find)

--- release/queue.c
+++ release/queue.c
@@ -6,12 +6,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct node_t{
+  void* val;
+  struct node_t* next;
+} node_t;
+
+typedef struct queue{
+  int len;
+  struct node_t* head;
+  struct node_t* tail;
+} queue;
+
 /*
  * Return an empty queue.
  */
 queue_t
 queue_new() {
-    return (queue_t)0;
+  queue_t new_q = (queue_t)malloc(sizeof(queue));
+  if (new_q == NULL){
+    return NULL;
+  }
+  new_q->len = 0;
+  new_q->head = NULL;
+  new_q->tail = NULL;
+  return new_q;
 }
 
 /*
@@ -20,7 +38,22 @@ queue_new() {
  */
 int
 queue_prepend(queue_t queue, void* item) {
-    return 0;
+  node_t* new_node;
+  if (queue == NULL){
+    return -1;
+  }
+  new_node = (node_t*)malloc(sizeof(node_t));
+  if (new_node == NULL){
+    return -1;
+  }
+  new_node->next = queue->head;
+  new_node->val = item;
+  if (queue->head == NULL) {
+    queue->tail = new_node;
+  }
+  queue->head = new_node;
+  queue->len++;
+  return 0;
 }
 
 /*
@@ -29,7 +62,25 @@ queue_prepend(queue_t queue, void* item) {
  */
 int
 queue_append(queue_t queue, void* item) {
-    return 0;
+  node_t* new_node;
+  if (queue == NULL){
+    return -1;
+  }
+  new_node = (node_t*)malloc(sizeof(node_t));
+  if (new_node == NULL){
+    return -1;
+  }
+  new_node->next = NULL;
+  new_node->val = item;
+  if (queue->head == NULL){
+    queue->head = new_node;
+    queue->tail = new_node;
+  } else {
+    queue->tail->next = new_node;
+    queue->tail = new_node;
+  }
+  queue->len++;
+  return 0;
 }
 
 /*
@@ -38,7 +89,22 @@ queue_append(queue_t queue, void* item) {
  */
 int
 queue_dequeue(queue_t queue, void** item) {
-    return 0;
+  node_t* temp;
+  if (queue == NULL || queue->len == 0){
+    return -1;
+  }
+  *item = queue->head->val;
+  if (queue->len == 1){
+    free(queue->head);
+    queue->head = NULL;
+    queue->tail = NULL;
+  } else {
+    temp = queue->head;
+    queue->head = queue->head->next;
+    free(temp);
+  }
+  queue->len--;
+  return 0;
 }
 
 /*
@@ -57,7 +123,11 @@ queue_iterate(queue_t queue, func_t f, void* item) {
  */
 int
 queue_free (queue_t queue) {
-    return 0;
+  if (queue == NULL || queue->len != 0){
+    return -1;
+  }
+  free(queue);
+  return 0;
 }
 
 /*
@@ -65,7 +135,7 @@ queue_free (queue_t queue) {
  */
 int
 queue_length(queue_t queue) {
-    return 0;
+    return queue->len;
 }
 
 
